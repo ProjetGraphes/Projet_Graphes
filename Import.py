00cc7522b9099abc17d3c3a63f50f70fb355f544
jeppoiii <129389582+jeppoiii@users.noreply.github.com>
2023-03-30 23:19:39 +0200

    Update Import.py

--- Import.py
+++ Import.py
@@ -4,22 +4,24 @@ import sys
 from dotenv import load_dotenv
 import os
 
-db = mysql.connector.connect(
+mydb = mysql.connector.connect(
   host=os.getenv('DB_HOST'),
   user=os.getenv('DB_USER'),
   password=os.getenv('DB_PASSWORD'),
   database=os.getenv('DB_NAME')
 )
 cursor = db.cursor()
-csv_data = csv.reader(open('CSV_Streamers'))
-headers = next(csv_data)
+headers = next(csv_file)
 
 print("Importation en cours...")
-for row in csv_data:
-    print(row)
-    cursor.execute("INSERT INTO streamers (streamers_login, streamers_twitch_id) VALUES (%s, %s)", row )
+with open('CSV_Streamers') as csv_file:
+    csvfile = csv.reader(csv_file, delimiter=",")
+    all_value = []
+    for row in csvfile:
+        value = (row[0], row[2])
+        all_value.append(value)
+        cursor.execute("INSERT INTO streamers (streamers_login, streamers_twitch_id) VALUES (%s, %s)", all_value )
 
 db.commit()
 cursor.close()
 print ("Importation fini :)")
-

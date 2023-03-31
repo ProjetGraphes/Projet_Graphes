387e7fe5a0668089ffd1dd137262ebfcef857058
FanuelM <40004506@parisnanterre.fr>
2023-03-31 19:48:35 +0200

    Affichage du graphe

--- Main.py
+++ Main.py
@@ -1,24 +1,17 @@
 import csv
-from dotenv import load_dotenv
-import os
-import requests
-import mysql.connector
+import random
+import networkx as nx
+import matplotlib.pyplot as plt
 
-NOMBRE_STREAMERS = 993
-LIEN_STREAMERS = 1000
-
-load_dotenv()
-headers = {
-    'Client-ID': os.getenv('CLIENT_ID'),
-    'Authorization': 'Bearer ' + os.getenv('CLIENT_SECRET')
-}
-
-mydb = mysql.connector.connect(
-  host=os.getenv('DB_HOST'),
-  user=os.getenv('DB_USER'),
-  password=os.getenv('DB_PASSWORD'),
-  database=os.getenv('DB_NAME')
-)
+#NIVEAU_CONFIANCE : Pour un niveau de confiance de 95 %
+#PROPORTION_INTERET : Estimation conservatrice de 0,5 (car on ne sait pas si deux streamers ont publics similaires)
+#TAILLE_ECHANTILLON : Formule de Cochran
+NOMBRE_STREAMERS = 50
+SEUIL_ARETE = 10000
+NIVEAU_CONFIANCE = 1.96
+MARGE_ERREUR = 0.01
+PROPORTION_INTERET = 0.5
+TAILLE_ECHANTILLON = round(((NIVEAU_CONFIANCE ** 2) * PROPORTION_INTERET * (1 - PROPORTION_INTERET))/(MARGE_ERREUR ** 2))
 
 def affiche_matrice(matrice):
     for i in range(len(matrice)):
@@ -26,39 +19,80 @@ def affiche_matrice(matrice):
             print("    {}    ".format(matrice[i][j]), end='|')
         print()
 
-#Dans la diagonale, 3 informations sur le streamer (dans cet ordre) : ID, login et nombre de followers
+#Dans la diagonale, 2 informations sur le streamer (dans cet ordre) : login et nombre de followers
 def init_matrice():
     with open('CSV_Streamers.csv', mode='r', encoding='utf-8') as csv_file:
         reader = csv.DictReader(csv_file, delimiter=',')
         matrice = []
         
-        for i in range(3):
+        for i in range(NOMBRE_STREAMERS):
             ligne = []
             for j in range(i+1):
                 if(i == j):
                     infos_streamer = next(reader)
-                    ligne.append([int(infos_streamer['ID']), infos_streamer['Channel'], int(infos_streamer['Followers'])])
+                    ligne.append([infos_streamer['Channel'], int(infos_streamer['Followers'])])
                 else:
                     ligne.append(0)
             matrice.append(ligne)
+    
+    #Initialisation des followers en communs entre deux streamers
+    for i in range(1, len(matrice)):
+        for j in range(i):
+            matrice[i][j] = estimation_followers_commun(matrice[i][i][1], matrice[j][j][1])
+
     return matrice
 
+#Utilisation d'un échantillonnage aléatoire simple pour avoir estimation du nombre de followers communs à 2 streamers
+def estimation_followers_commun(nb_a, nb_b):
+    #Création échantillons à partir de sélection aléatoire de TAILLE_ECHANTILLON chiffres parmi une liste de taille nb_a/nb_b
+    echantillon_a = random.sample(range(nb_a), TAILLE_ECHANTILLON)
+    echantillon_b = random.sample(range(nb_b), TAILLE_ECHANTILLON)
 
-def lien_streamer(id_a, id_b):
-    nb_followers_commun = 0
+    nb_followers_en_commun = len(set(echantillon_a).intersection(set(echantillon_b)))
+
+    #Calcul de l'estimation en utilisant un facteur de correction
+    #estimation = round(followers_en_commun * (nb_a / len(echantillon_a)) * (nb_b / len(echantillon_b)))
+    estimation = round((((nb_followers_en_commun / TAILLE_ECHANTILLON) * nb_a) + ((nb_followers_en_commun / TAILLE_ECHANTILLON) * nb_b))/2 )
+
+    return estimation
+
+################################
+#Fonctions de création du graphe
+def charger_graphe(matrice):
+    G = nx.Graph()
     
+    #Initialisation des sommets et création d'un tableau pour stocker leur taille
+    tailles = []
+    for i in range(len(matrice)):
+        G.add_node(matrice[i][i][0])
+        tailles.append(matrice[i][i][1]/10000)
 
+    #Initialisation des arêtes
+    for i in range(1, len(matrice)):
+        for j in range(i):
+            if(matrice[i][j] > SEUIL_ARETE):
+                G.add_edge(matrice[i][i][0],matrice[j][j][0], width=100)
+    
+    return [G, tailles]
 
-    return nb_followers_commun
+def generation_couleur_aleatoire(G):
+    color_map = []
+    for node in G:
+        if random.randint(0,1) == 0:
+            color_map.append('blue')
+        else: 
+            color_map.append('green')
+    return color_map
 
-'''matrice = init_matrice()
-affiche_matrice(matrice)'''
-data_json = requests.get("https://api.twitch.tv/helix/users/follows?to_id=55828551", headers=headers)
-data_dict = data_json.json()
+matrice = init_matrice()
+#affiche_matrice(matrice)
 
+tab_aux = charger_graphe(matrice)
+G = tab_aux[0]
+tailles = tab_aux[1]
+color_map = generation_couleur_aleatoire(G)          
 
-print(data_dict)
-'''tab1 = [1, 2, 3, 4, 5]
-tab2 = [3, 4, 5, 6, 7]
+nx.draw(G, with_labels=True, node_size=tailles, node_color=color_map, edge_color='red', width=0.5)
 
-print(set(tab1))'''
+plt.savefig('Graphes.png', dpi=300)
+plt.show()

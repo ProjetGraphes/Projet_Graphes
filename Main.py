dc3cf77b1a2809bf6be050e5fd11383e029b346d
FanuelMehari <119321322+FanuelMehari@users.noreply.github.com>
2023-04-04 14:08:08 +0200

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,18 +1,20 @@
 import csv
 import random
 import networkx as nx
+from networkx.algorithms.community import greedy_modularity_communities
 import matplotlib.pyplot as plt
 
 #NIVEAU_CONFIANCE : Pour un niveau de confiance de 95 %
 #PROPORTION_INTERET : Estimation conservatrice de 0,5 (car on ne sait pas si deux streamers ont publics similaires)
 #TAILLE_ECHANTILLON : Formule de Cochran
-NOMBRE_STREAMERS = 100
+NOMBRE_STREAMERS = 50
 SEUIL_ARETE = 10000
 NIVEAU_CONFIANCE = 1.96
 MARGE_ERREUR = 0.01
 PROPORTION_INTERET = 0.5
 TAILLE_ECHANTILLON = round(((NIVEAU_CONFIANCE ** 2) * PROPORTION_INTERET * (1 - PROPORTION_INTERET))/(MARGE_ERREUR ** 2))
 
+#FONCTIONS DE CREATION DE LA MATRICE
 def affiche_matrice(matrice):
     for i in range(len(matrice)):
         for j in range(len(matrice[i])):
@@ -50,14 +52,13 @@ def estimation_followers_commun(nb_a, nb_b):
 
     nb_followers_en_commun = len(set(echantillon_a).intersection(set(echantillon_b)))
 
-    #Calcul de l'estimation en utilisant un facteur de correction
-    #estimation = round(followers_en_commun * (nb_a / len(echantillon_a)) * (nb_b / len(echantillon_b)))
-    estimation = round((((nb_followers_en_commun / TAILLE_ECHANTILLON) * nb_a) + ((nb_followers_en_commun / TAILLE_ECHANTILLON) * nb_b))/2 )
+    #Calcul de l'estimation
+    estimation = round(nb_followers_en_commun * nb_a / TAILLE_ECHANTILLON)
 
     return estimation
 
-################################
-#Fonctions de création du graphe
+
+#FONCTIONS DE CREATION DU GRAPHE
 def charger_graphe(matrice):
     G = nx.Graph()
     
@@ -75,24 +76,29 @@ def charger_graphe(matrice):
     
     return [G, tailles]
 
-def generation_couleur_aleatoire(G):
+def detection_communautes(G):
+    communautes = greedy_modularity_communities(G)
+    print(communautes)
+    couleurs = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'orange', 'grey', 'cyan', 'magenta']
     color_map = []
-    for node in G:
-        if random.randint(0,1) == 0:
-            color_map.append('blue')
-        else: 
-            color_map.append('green')
+
+    for sommets in G.nodes():
+        for cle, communautes in enumerate(communautes):
+            if sommets in communautes:
+                color_map.append(couleurs[cle])
+                break
+
     return color_map
 
 matrice = init_matrice()
-#affiche_matrice(matrice)
+affiche_matrice(matrice)
 
 tab_aux = charger_graphe(matrice)
 G = tab_aux[0]
 tailles = tab_aux[1]
-color_map = generation_couleur_aleatoire(G)          
+color_map = detection_communautes(G)
 
-nx.draw(G, with_labels=True, font_size=5, font_color='white', node_size=tailles, node_color=color_map, edge_color='red' ,width=0.1)
-plt.rcParams['savefig.facecolor'] = '#000000' # Set background color to black
+nx.draw(G, with_labels=True, node_size=tailles, node_color=color_map, edge_color='red', width=0.5)
+
 plt.savefig('Graphes.png', dpi=300)
 plt.show()
